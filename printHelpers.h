233209de9867bf867e3dc059496281c2f50e3dc3
rob tillaart <rob.tillaart@gmail.com>
2020-07-06 20:46:00 +0200

    0.1.1 added toBytes()

--- printHelpers.h
+++ printHelpers.h
@@ -3,15 +3,12 @@
 //    FILE: printHelpers.h
 //  AUTHOR: Rob Tillaart
 //    DATE: 2018-01-21
-// VERSION: 0.1.0
+// VERSION: 0.1.1
 // PUPROSE: Arduino library to help formatting for printing. 
 //     URL: https://github.com/RobTillaart/printHelpers
 
-// print64 note
-// buffer size 66 will work for base 2 -36
-// buffer size 34 will work for base 4 -36
-// buffer size 24 will work for base 8 -36
-// buffer size 22 will work for base 10 - 36
+
+// 24 is a pretty safe minimum
 
 #ifndef PRINTBUFFERSIZE
 #define PRINTBUFFERSIZE 66
@@ -22,6 +19,18 @@
 // not usable in multi-threading environments (use with care)
 
 char __printbuffer[PRINTBUFFERSIZE];
+
+
+////////////////////////////////////////////////////////////
+//
+// PRINT 64 BIT 
+//
+
+// print64 note
+// buffer size 66 will work for base 2 -36
+// buffer size 34 will work for base 4 -36
+// buffer size 24 will work for base 8 -36
+// buffer size 22 will work for base 10 - 36
 
 char * print64(int64_t n, uint8_t base = 10)
 {
@@ -107,6 +116,13 @@ char * print64(uint64_t n, uint8_t base = 10)
   return buf;
 }
 
+////////////////////////////////////////////////////////////
+//
+// SCIENTIFIC NOTATIION
+//
+
+// typical buffer size for 8 byte double is 22 bytes 
+// 15 bytes mantissa, sign dot E-xxx
 char * scieng(double number, uint8_t digits, uint8_t em)
 {
   char * buf = __printbuffer;
@@ -221,5 +237,52 @@ void sci(Stream &str, double f, uint8_t digits)
 {
   str.print(sci(f, digits));
 }
+
+////////////////////////////////////////////////////////////
+//
+// toBytes 
+//
+
+// official support to UDA == 1024^12
+// kilo mega giga tera peta exa (1024^6)
+// zetta yotta xona weka vunda uda (1024^12)
+//
+// (treda Byte == TDB is the next one and it is 2 char 
+// so codewise difficult and as it is seldom used, support stops there.
+// 
+// To have some support the code uses lowercase for the next 8 levels
+// treda sorta rinta quexa pepta ocha nena minga luma (1024 ^21 ~~ 10^63)
+char * toBytes(double val, uint8_t decimals = 2)
+{
+  static char buf[12];
+  char t[] = " KMGTPEZYXWVUtsrqponml";  
+  uint8_t i = 0;
+  if (isinf(val)) return "<inf>";
+
+  while(val >= 1024)
+  {
+    val /= 1024;
+    i++;
+  }
+  uint8_t length = 7;
+  if (i == 0) decimals = 0;
+  if (decimals > 3) decimals = 3;
+  dtostrf(val, length, decimals, buf);
+
+  uint8_t pos = strlen(buf);
+  if (i <= strlen(t))
+  {
+    if (i > 0) buf[pos++] = ' ';
+    buf[pos++] = t[i];
+    buf[pos++] = 'B';
+    buf[pos] = 0;
+  }
+  else
+  {
+    // TODO   e.g. E99 B
+  }
+  return buf;
+}
+
 
 // -- END OF FILE --
